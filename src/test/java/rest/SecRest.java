7037a9a696be75d811b28afefdad07689ea332e1
mkhatre <51029984+mkhatre@users.noreply.github.com>
2019-05-26 00:15:34 +0530

    restassured gherkin example

--- src/test/java/rest/SecRest.java
+++ src/test/java/rest/SecRest.java
@@ -1,14 +1,38 @@
 package rest;
 import org.junit.*;
+import static io.restassured.RestAssured.*;
+//import static io.restassured.RestAssured.given;
 
 public class SecRest {
+
+	public String baseURI = "http://restapi.demoqa.com/utilities/weather/city/";
+	public String city = "Hyderabad";
 	
 	@Before
 	public void setup() {
-		public String baseURI = "";
-		public String basePath = "";
+//		public String baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+//		public String basePath = "";
 		
 		
+	}
+	
+	@Test
+	public void test_URI_correct() {
+		given().
+		when().
+			get(baseURI+city).
+		then().
+			statusCode(200);
+
+	}
+	
+	@Test
+	public void test_content_type() {
+		given().
+		when().
+			get(baseURI+city).
+		then().
+			contentType("application/json");
 	}
 	
 
